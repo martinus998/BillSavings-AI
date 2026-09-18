e8a55326796f7827974f5cd4fab9fefef338d745
martinus998 <leskomartin94@gmail.com>
2026-09-18 09:40:17 +0200

    Stop exposing personal support email publicly

--- customer-help.js
+++ customer-help.js
@@ -1,6 +1,6 @@
 // Set only confirmed public support details and the live Stripe-hosted portal link.
 // Empty values deliberately render no unconfigured links.
-const BILL_SUPPORT_EMAIL = 'Martinus998@azet.sk';
+const BILL_SUPPORT_EMAIL = '';
 const BILL_PORTAL_URL = 'https://billing.stripe.com/p/login/bJedR869sfGbdtOeIC1sQ00';
 
 (() => {
